ab7bee68026d15cc8a5ab9f8e9ec46cb7350a285
Pinnamaraju <pinnamaraju.appalaraju@gmail.com>
2017-12-31 22:08:15 +0530

    Commited for build by Raju on 31st DEC

--- FirstApplication/src/FirstClass.java
+++ FirstApplication/src/FirstClass.java
@@ -4,8 +4,6 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello How Are you Siddu    ??");
-System.out.println("I am not ok");
-System.out.println("What about you");
 	}
 
 }
